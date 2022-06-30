46b42aedeefc6f08b750ad5a15dc70841f3c45f3
George Osodo <gjeff103@gmail.com>
2022-06-30 14:40:55 -0700

    C program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,16 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
+ *  main - Entry point
+ * Description:use printf
+ * * Return: Always 0 (Success)
  *
- *  * main - Print a string
- *
- *   * Description: use printf
- *
- *    * Return: 0
- *
- *     */
+ */
 
 int main(void)
 
 {
 
-		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
 
