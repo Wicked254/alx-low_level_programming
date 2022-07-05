5f8c6301902a550e761d8213f5582e755544d25a
George Osodo <gjeff103@gmail.com>
2022-07-05 07:11:25 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two numbers from input
+ * @a: first aparamet
+ *  @b: second parameter
+ *  Description: adds two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
